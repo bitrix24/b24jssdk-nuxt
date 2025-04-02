6cf7d21c4ef1cb3f37e7b3596b62f1002c5cd2be
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-04-02 13:06:03 +0300

    chore(dep): improve

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,11 @@
-// @ts-check
 import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 
-// Run `npx @eslint/config-inspector` to inspect the resolved config interactively
 export default createConfigForNuxt({
-	features: {
-		tooling: true,
-		stylistic: false,
-	},
-	dirs: {
-		src: [
-			'./playground',
-		],
-	},
+  features: {
+    tooling: true,
+    stylistic: {
+      commaDangle: 'never',
+      braceStyle: '1tbs'
+    }
+  }
 })
