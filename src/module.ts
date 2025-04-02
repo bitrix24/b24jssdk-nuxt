f8a6494a24fa1192b6aa681d67615056eed2ba2d
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-04-02 13:06:18 +0300

    chore(lint): improve

--- src/module.ts
+++ src/module.ts
@@ -3,21 +3,20 @@ import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
 export type ModuleOptions = object
 
 export default defineNuxtModule<ModuleOptions>({
-	meta: {
-		name: '@bitrix24/b24jssdk-nuxt',
-		version: '0.1.0',
-		configKey: 'B24JsSdkNuxt',
-		compatibility: {
-			nuxt: '>=3.13.0'
-		}
-	},
-	defaults: {},
-	setup(_options, _nuxt)
-	{
-		const resolver = createResolver(import.meta.url)
-		
-		addPlugin({
-			src: resolver.resolve('./runtime/plugin')
-		})
-	},
+  meta: {
+    name: '@bitrix24/b24jssdk-nuxt',
+    version: '0.1.0',
+    configKey: 'B24JsSdkNuxt',
+    compatibility: {
+      nuxt: '>=3.13.0'
+    }
+  },
+  defaults: {},
+  setup(_options, _nuxt) {
+    const resolver = createResolver(import.meta.url)
+
+    addPlugin({
+      src: resolver.resolve('./runtime/plugin')
+    })
+  }
 })
--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,23 +1,20 @@
-import { defineNuxtPlugin } from '#app'
 import { initializeB24Frame } from '@bitrix24/b24jssdk'
+import { defineNuxtPlugin } from '#app'
 
 /**
  * Connecting api.bitrix24 (singleton)
  */
-export default defineNuxtPlugin((_nuxtApp) =>
-{
-	/**
-	 * @memo skip Plugin on server ////
-	 */
-	if(import.meta.server)
-	{
-		return;
-	}
-	
-	return {
-		provide: {
-			initializeB24Frame,
-		}
-	}
-})
+export default defineNuxtPlugin((_nuxtApp) => {
+  /**
+   * @memo skip Plugin on server ////
+   */
+  if (import.meta.server) {
+    return
+  }
 
+  return {
+    provide: {
+      initializeB24Frame
+    }
+  }
+})
