5a493c400d7b11cafa99f98fece639442189bdbb
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-04-02 13:20:19 +0300

    chore(module): improve

--- src/module.ts
+++ src/module.ts
@@ -5,18 +5,16 @@ export type ModuleOptions = object
 export default defineNuxtModule<ModuleOptions>({
   meta: {
     name: '@bitrix24/b24jssdk-nuxt',
-    version: '0.1.0',
+    version: '0.1.2',
     configKey: 'B24JsSdkNuxt',
     compatibility: {
-      nuxt: '>=3.13.0'
+      nuxt: '>=3.16.0'
     }
   },
   defaults: {},
-  setup(_options, _nuxt) {
+  async setup(_options, _nuxt) {
     const resolver = createResolver(import.meta.url)
 
-    addPlugin({
-      src: resolver.resolve('./runtime/plugin')
-    })
+    addPlugin({ src: resolver.resolve('./runtime/plugin') })
   }
 })
